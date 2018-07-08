64fb93dded3a34984805aa90ad34e1bc7aab1d01
“Richard Middleton” <Richard@richardmiddleton.me>
2018-07-08 11:47:43 +0200

    server js

--- server.js
+++ server.js
@@ -10,25 +10,26 @@ const app = express();
 // Bodyparser Middleware
 app.use(bodyParser.json());
 
-
 // DB Config
 const db = require('./config/keys').mongoURI;
 
 // Connect to Mongo
-mongoose.connect(db)
-    .then( () => console.log('MongoDB Connected'))
-    .catch(err => console.log(err));
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
 // Use Routes
-app.use('/api/items', items)
+app.use('/api/items', items);
 
-// Check if in production
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 const port = process.env.PORT || 5000;
